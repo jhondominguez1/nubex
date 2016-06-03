fa5fe6971d56fd3574133a943af21a54d55ef2eb
MichaelStevenDelgado <miesdeme@gmail.com>
2016-06-03 08:25:39 -0500

    se realizo un comentario en el formulario de ingreso administrador Michael

---
+++ formulario3.php
@@ -0,0 +1,8 @@
+<?php
+echo "hola jhon soy michael";
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
